01c7ed3a40eae3e4fafc8056997103d701e9fcec
Sarath <sarath.hacks@gmail.com>
2016-04-10 19:14:08 +0530

    Updated

---
+++ app/es6/app.es6
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+const AppComponent = Vue.extend({
+  template: '<h2>Hello World</h2>',
+});
+
+export default AppComponent
--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var style = require('./app/style.css');
 
 var Person = require('./app/es6/person.es6').default;
 
+
 console.log(style);
 
 console.log(message);
@@ -15,3 +16,15 @@ var h1 = $('<h1></h1>').text(message);
 $('body').css('background','red').prepend(h1);
 
 var p = new Person();
+
+
+// Vue app
+
+var Vue = require("vue");
+var AppComponent = require("./app/es6/app.es6");
+new Vue({
+  el: 'body',
+  components: {
+    'app-component': AppComponent
+  }
+});
